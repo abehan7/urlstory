92896e9d643b8f065294a05053d615c3a97efe6d
한준규 <hanjk13262@gmail.com>
2022-03-03 16:33:31 +0900

    fix: 폰트 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
+import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store2 from "./store/store2";
 import { createGlobalStyle } from "styled-components";
-import { lazy } from "@loadable/component";
-const App = lazy(() => import("./App"));
 
 // import DataProvider from "./redux/StoreT";
 
@@ -14,17 +12,10 @@ const GlobalStyle = createGlobalStyle`
 
 position: relative;
  
-@font-face {
-  font-family: "Pretendard-Regular";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
-    format("woff");
-  font-weight: 400;
-  font-style: normal;
-}
+ 
 
 * {
-  font-family: "Pretendard-Regular";
-
+  font-family: "Pretendard";
   font-weight: bold;
 }
 a {
@@ -32,28 +23,15 @@ a {
 }
 h2 {
   font-size: 30px;
-  /* font-size: 20px; */
   font-weight: bold;
 }
 
 `;
 
 ReactDOM.render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <Provider store={store2}>
-      <GlobalStyle />
-      <App />
-    </Provider>
-  </Suspense>,
+  <Provider store={store2}>
+    <GlobalStyle />
+    <App />
+  </Provider>,
   document.getElementById("urlseries")
 );
-
-// ===수정 전 코드===
-
-// ReactDOM.render(
-//   <Provider store={store2}>
-//     <GlobalStyle />
-//     <App />
-//   </Provider>,
-//   document.getElementById("ururl")
-// );
